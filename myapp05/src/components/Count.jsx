8b3abfa80523b33cb42dff02393f376aacce1afd
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-10-28 21:13:18 +0200

    Create Count component

---
+++ myapp05/src/components/Count.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Count(props){
+    return(
+        <div className="container" >
+            <h1 className="truethness">{props.number}</h1>
+        </div>
+    )
+}
