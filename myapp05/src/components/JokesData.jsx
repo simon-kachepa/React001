2d85d55c66a0aa7b059277a6eaf9813142f73c15
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-10-28 21:18:12 +0200

    Create JokesData component

---
+++ myapp05/src/components/JokesData.jsx
@@ -0,0 +1,22 @@
+export default [
+    {
+        setup: "What do you do when your hot pants catch on fire?",
+        punchline: "Put them out with your pantyhose."
+    },
+    {
+        setup: "WWhy don’t chickens wear underwear?",
+        punchline: "Because their pecker is on their face"
+    },
+    {
+        setup: "What’s green and has wheels?",
+        punchline: "Grass! I lied about the wheels."
+    },
+    {
+        setup: "What was E.T short for?",
+        punchline: "Because he had little legs."
+    },
+    {
+        setup: "Why’d the old man fall down the well?",
+        punchline: "Because he couldn’t see that well!"
+    }
+]
