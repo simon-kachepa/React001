8aa6c4f41bf9378756671f81d5164875dd35df3e
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-10-28 21:01:36 +0200

    Create Boxes component

---
+++ myapp05/src/components/Boxes.jsx
@@ -0,0 +1,34 @@
+export default [
+    {
+        id: 1,
+        on: true
+    },
+    {
+        id: 2,
+        on: false
+    },
+    {
+        id: 3,
+        on: true
+    },
+    {
+        id: 4,
+        on: false
+    },
+    {
+        id: 5,
+        on: true
+    },
+    {
+        id: 6,
+        on: true
+    },
+    {
+        id: 7,
+        on: false
+    },
+    {
+        id: 8,
+        on: true
+    },
+]
