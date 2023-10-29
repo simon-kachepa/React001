2b7fddbcc08b17b5906c3d9f2b9d1bb9f0e98082
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-10-29 23:24:51 +0200

    Update Joke component

--- myapp05/src/components/Joke.jsx
+++ myapp05/src/components/Joke.jsx
@@ -1,10 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Joke(props){
+    const [isShown, setIsSHown] = useState(false)
+
+    function toggleIsShown(){
+        setIsSHown(prevState => !prevState) 
+    }
     return(
         <div>
-            <p>{props.setup}</p>
-            <p>{props.punchline}</p>
+            <p>Setup:{props.setup}</p>
+            {isShown && <p>Punchline: {props.punchline}</p>}
+            <button onClick={toggleIsShown}>{isShown ? "Hide" : "Show"} Punchline</button>
+            <hr />
         </div>
     )
 }
